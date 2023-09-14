8d6be5f5b231ac660d4355f154f6486f3539f62b
kaser1997 <kaser.slaiby@gmail.com>
2023-09-14 16:42:07 +0300

    modified

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,10 @@ const config: CapacitorConfig = {
   appName: 'angular-mobile',
   webDir: 'dist/Angular-Mobile',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+    url:  'https://14742.2645.161366.6:4200',
+    cleartext: true,
+  },
 };
 
 export default config;
